ebe33e79ffec811e0384325e448521a1e8015872
Westbrook <WestbrookE@si.edu>
2026-07-01 11:55:04 -0400

    adding fill missign metadata to list of deprecated functions

--- R/utl_mg_rls_deprecated.R
+++ R/utl_mg_rls_deprecated.R
@@ -152,7 +152,6 @@ utl_mg_load_excel_deprecated <- function(filepath, output_table, sheet_name){
 #'
 #' @returns A dataframe with taxonomic levels as columns
 #' @export
-#' @keyword internal
 #' @examples
 #' df <- utl_rls_load_excel_deprecated("inst/extdata/test_rls_data_EPA.xlsx", sheet = "DATA") |>
 #'  dplyr::filter(Method == "1" | Method == "2")
@@ -222,7 +221,6 @@ utl_join_taxonomy_by_scientific_name_deprecated <- function(df,
 #'
 #' @returns dataframe of taxonomic classifications and taxonomic ID (usually Aphia ID)
 #' @export
-#' @keyword internal
 #' @examples
 #' utl_get_taxonomy_by_scientific_name(c("Gastropoda", "Dardanus venosus"))
 #' utl_get_taxonomy_by_scientific_name("Dardanus venosus", taxonomic_levels = "phylum")
@@ -289,6 +287,93 @@ db_get_taxonomy_by_scientific_name_deprecated <- function(scientific_names,
   })
 }
 
+
+#' Filling missing metadata columns based on other processed RLS data
+#'
+#' For columns "vis", "direction", "time", and "photoquadrats", the function
+#' converts "BLANK" to NA and checks processed L2 data for fill values from
+#' the same survey.
+#'
+#' @param df the data frame to check for missing metadata.
+#'
+#' @returns dataframe with all metadata
+#' @export
+#'
+#'
+#'
+#'
+utl_rls_fill_missing_metadata_deprecated <- function(df){
+
+  columns <- c("sample_event_id", "vis", "direction", "time", "photoquadrats")
+
+  ids <- unique(df$sample_event_id)
+
+  df_in <- df %>%
+    mutate(vis = case_when(
+      vis == "BLANK" ~ NA,
+      T ~ vis
+    )) %>%
+    mutate(direction = case_when(
+      direction == "BLANK" ~ NA,
+      T ~ direction
+    )) %>%
+    mutate(photoquadrats = case_when(
+      photoquadrats == "BLANK" ~ NA,
+      T ~ photoquadrats
+    ))
+
+  if(is.character(df_in$time)){
+    df_in <- df_in %>%
+      mutate(time = case_when(
+        time == "BLANK" ~ NA,
+        T ~ time
+      ))
+  }
+
+  fill_options <- marinegeo.utils::db_marinegeo_L2("reef-life-survey-data-marinegeo-v1") %>%
+    filter(sample_event_id %in% ids) %>%
+    select(all_of(columns)) %>%
+    distinct() %>%
+    mutate(vis = case_when(
+      vis == "BLANK" ~ NA_character_,
+      T ~ vis
+    )) %>%
+    mutate(direction = case_when(
+      direction == "BLANK" ~ NA_character_,
+      T ~ direction
+    )) %>%
+    mutate(photoquadrats = case_when(
+      photoquadrats == "BLANK" ~ NA_character_,
+      T ~ photoquadrats
+    )) %>%
+    mutate(time = case_when(
+      time == "BLANK" ~ NA_character_,
+      T ~ time
+    )) %>%
+    collect() %>%
+    group_by(sample_event_id) %>%
+    summarize(vis = first(vis, na_rm = T),
+              direction = first(direction, na_rm = T),
+              photoquadrats = first(photoquadrats, na_rm = T),
+              time = first(time, na_rm = T))
+
+
+  if(is.numeric(df_in$vis)){
+    df_in <- df_in %>%
+      mutate(vis = as.character(vis))
+  }
+
+  if(!is.character(df_in$time)){
+    df_in <- df_in %>%
+      mutate(time = as.character(time))
+  }
+
+  df_out <- df_in %>%
+    rows_patch(fill_options, by = "sample_event_id")
+
+  return(df_out)
+
+}
 
 
 
